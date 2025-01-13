f0ced41465bd56b0667ccfdb9e8b1c6d5264e0db
Tharaka De Silva <tharaka.uo@gmail.com>
2025-01-13 21:17:32 +0100

    Refactor spawner.js to enhance output logging and maintainability
    - Changed the definition of `spawnAndPrintOutput` to an arrow function for consistency.
    - Updated logging to include the process name in stdout, stderr, and exit code messages for better traceability.
    - Ensured the function returns the child process instance for potential further use.

--- spawner.js
+++ spawner.js
@@ -1,21 +1,23 @@
 const { spawn } = require('node:child_process');
 
-function spawnAndPrintOutput(process, args) {
+const spawnAndPrintOutput = (process, args) => {
   const child = spawn(process, args);
 
   child.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+    console.log(`(${process}): stdout: ${data}`);
   });
-  
+
   child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
+    console.error(`(${process}): stderr: ${data}`);
   });
-  
+
   child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log(`(${process}): child process exited with code ${code}`);
   });
+
+  return child;
 }
 
 spawnAndPrintOutput('node', ['printer.js']);
-
 spawnAndPrintOutput('xcrun', ['simctl', 'boot', '82170374-AAAC-4E77-A1FF-6C7675EC9E05']);
+
