780837e6db41a79b557267663e3cfee1a27d393f
Tharaka De Silva <tharaka.uo@gmail.com>
2025-01-13 21:13:39 +0100

    Refactor spawner.js to improve process management
    - Introduced a new function `spawnAndPrintOutput` to handle spawning child processes and logging their output.
    - Enhanced error handling by adding stderr output logging.
    - Updated the script to spawn both 'printer.js' and 'xcrun' processes using the new function.

--- spawner.js
+++ spawner.js
@@ -1,15 +1,21 @@
 const { spawn } = require('node:child_process');
 
-const ls = spawn('node', [`${__dirname}/printer.js`]);
+function spawnAndPrintOutput(process, args) {
+  const child = spawn(process, args);
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
 
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
+spawnAndPrintOutput('node', ['printer.js']);
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+spawnAndPrintOutput('xcrun', ['simctl', 'boot', '82170374-AAAC-4E77-A1FF-6C7675EC9E05']);
